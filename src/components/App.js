afa30a16f1634b571c45ccfe7cc7a40611f321c2
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2023-07-29 06:56:36 +0200

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-
+import ReactDOM from "react-dom";
+import Counter from './Counter';
 
 
 export default class App extends Component {
-
+   
+    render(){
+        return (
+            <Counter />
+        )
+    }
 }
